b94fe5c489577493e24a349e45dc56080a658f73
Abbas Attarwala <abbasdgr8st@gmail.com>
2015-03-23 01:28:55 +0530

    Refactored Part-1

--- src/main/java/com/abbasdgr8/vendingmachine/VendingMachine.java
+++ src/main/java/com/abbasdgr8/vendingmachine/VendingMachine.java
@@ -2,14 +2,14 @@ package com.abbasdgr8.vendingmachine;
 
 import java.util.Collection;
 import java.util.Set;
+import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.abbasdgr8.vendingmachine.collections.CoinDispenser;
 import com.abbasdgr8.vendingmachine.constants.Denomination;
 import com.abbasdgr8.vendingmachine.exceptions.NoSuchDenominationException;
 import com.abbasdgr8.vendingmachine.model.Coin;
-import com.abbasdgr8.vendingmachine.repository.CoinBank;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * 
@@ -18,22 +18,21 @@ import java.util.logging.Logger;
  */
 public class VendingMachine {
     
-    private final CoinBank coinBank = CoinBank.getInstance();
     private final CoinDispenser coinDispenser = CoinDispenser.getInstance();
     
     public Collection<Coin> getOptimalChangeFor(int pence) throws NoSuchDenominationException {
         
-        coinBank.setUnlimitedSupplyOfCoins(true);
-        
+        pence = (pence < 0) ? 0 : pence;
         int remainingChange = pence;
+        
         Set<Integer> denominationKeySet = Denomination.denominationLookup.keySet();
         Integer[] sortedDenominationArray = denominationKeySet.toArray(new Integer[denominationKeySet.size()]);
         
-        for(int i = sortedDenominationArray.length - 1; i > 0; i--) {
+        for(int i = sortedDenominationArray.length - 1; i >= 0; i--) {
             int currentDenominationValue = Denomination.get(sortedDenominationArray[i]).getDenominationValue();
             if (remainingChange >= currentDenominationValue) {
                 int currentDenominationCoinCount = remainingChange / currentDenominationValue;
-                coinBank.releaseCoins(currentDenominationValue, currentDenominationCoinCount);
+                coinDispenser.releaseCoins(currentDenominationValue, currentDenominationCoinCount);
                 remainingChange = remainingChange - currentDenominationCoinCount * currentDenominationValue;
             }
         }
@@ -41,16 +40,20 @@ public class VendingMachine {
         return coinDispenser;
     }
     
+    
     public static void main(String[] args) {
         VendingMachine vendingMachine = new VendingMachine();
         Collection<Coin> coins = null;
         try {
-            coins = vendingMachine.getOptimalChangeFor(49);
-        } catch (NoSuchDenominationException ex) {
+            coins = vendingMachine.getOptimalChangeFor(738);
+            System.out.println(coins.size() + " coins in dispenser.");
+            Iterator<Coin> iterator = coins.iterator();
+            while (iterator.hasNext()) {
+                System.out.println(iterator.next());
+            }
+        } catch (Exception ex) {
             Logger.getLogger(VendingMachine.class.getName()).log(Level.SEVERE, "Something went wrong", ex);
         }
-        
-        System.out.println("Output >>>>>> " + coins.size());
     }
     
 }
--- src/main/java/com/abbasdgr8/vendingmachine/collections/CoinDispenser.java
+++ src/main/java/com/abbasdgr8/vendingmachine/collections/CoinDispenser.java
@@ -1,7 +1,10 @@
 package com.abbasdgr8.vendingmachine.collections;
 
-import com.abbasdgr8.vendingmachine.model.Coin;
+import java.util.Collection;
 import java.util.concurrent.ArrayBlockingQueue;
+
+import com.abbasdgr8.vendingmachine.exceptions.NoSuchDenominationException;
+import com.abbasdgr8.vendingmachine.model.Coin;
 
 /**
  * This is a real-life vending machine coin dispenser that has been implemented 
@@ -13,6 +16,7 @@ import java.util.concurrent.ArrayBlockingQueue;
 public final class CoinDispenser extends ArrayBlockingQueue<Coin> {
     
     private static CoinDispenser theCoinDispenser = new CoinDispenser(100);
+    private final CoinBank coinBank = CoinBank.getInstance();
     
     /**
      * A static 'instance' method that returns the same instance of {@link com.abbasdgr8.vendingmachine.collections.CoinDispenser}
@@ -23,12 +27,80 @@ public final class CoinDispenser extends ArrayBlockingQueue<Coin> {
     public static CoinDispenser getInstance() {
         return theCoinDispenser;
     }
+    
+    public synchronized Collection<Coin> releaseCoins(int coinDenomination, int numberOfCoinsToRelease) 
+                                            throws NoSuchDenominationException {
+        
+        Collection<Coin> coinsReleased = coinBank.withdraw(coinDenomination, numberOfCoinsToRelease);
+        
+        return coinsReleased;
+    }
 
     /**
      * The private constructor
      */
     private CoinDispenser(int capacity) {
         super(capacity);
+        coinBank.setUnlimitedSupplyOfCoins(true);
+    }
+   
+    /**
+     * A repository where the vending machine stores its coins. This is
+     * implemented as a singleton class and returns the same instance of the
+     * bank each time {@link com.abbasdgr8.vendingmachine.repository.CoinBank#getInstance()
+     * }
+     * is called.
+     *
+     * @author Abbas Attarwala
+     */
+    private static class CoinBank {
+
+        private boolean unlimitedSupplyOfCoins;
+        private static final CoinBank theCoinBank = new CoinBank();
+
+        /**
+         * A static 'instance' method that returns the same instance of
+         * {@link com.abbasdgr8.vendingmachine.repository.CoinBank} each time
+         * the method is called.
+         *
+         * @return
+         */
+        public static CoinBank getInstance() {
+            return theCoinBank;
+        }
+
+        public synchronized Collection<Coin> withdraw(int coinDenomination, int numberOfCoinsToRelease) throws NoSuchDenominationException {
+            if (unlimitedSupplyOfCoins) {
+                for (int i = 0; i < numberOfCoinsToRelease; i++) {
+                    Coin coin = new Coin(coinDenomination);
+                    theCoinDispenser.offer(coin);
+                }
+            } else {
+                // get coins from bank
+            }
+
+            return theCoinDispenser;
+        }
+
+        public synchronized boolean deposit(Collection<Coin> coinsToDeposit) {
+            return false;
+        }
+
+        /**
+         * Getter for setting whether the coinBank has an unlimited Supply Of
+         * Coins or not.
+         *
+         * @param unlimitedSupplyOfCoins
+         */
+        public void setUnlimitedSupplyOfCoins(boolean unlimitedSupplyOfCoins) {
+            this.unlimitedSupplyOfCoins = unlimitedSupplyOfCoins;
+        }
+
+        /**
+         * The private constructor
+         */
+        private CoinBank() {
+        }
     }
     
 }
