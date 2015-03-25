d51b180682aa194172d87e7d0a7db23b410bdc7e
Abbas Attarwala <abbasdgr8st@gmail.com>
2015-03-26 03:18:10 +0530

    Added test classes and a test inventory

---
+++ src/test/java/com/abbasdgr8/vendingmachine/VendingMachineTest.java
@@ -0,0 +1,12 @@
+package com.abbasdgr8.vendingmachine;
+
+/**
+ * A test-class that contains unit tests for the {@link com.abbasdgr8.vendingmachine.VendingMachine}
+ *
+ * @author Abbas Attarwala
+ */
+public class VendingMachineTest {
+    
+    
+    
+}
---
+++ src/test/java/com/abbasdgr8/vendingmachine/collections/CoinDispenserTest.java
@@ -0,0 +1,12 @@
+package com.abbasdgr8.vendingmachine.collections;
+
+/**
+ * A test-class that contains unit-tests for the {@link com.abbasdgr8.vendingmachine.collections.CoinDispenser}
+ *
+ * @author Abbas Attarwala
+ */
+public class CoinDispenserTest {
+    
+    
+    
+}
